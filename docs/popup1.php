df879d1c7e998af6520cf8434437ba2f1d56e937
richard-keasley <67803679+richard-keasley@users.noreply.github.com>
2020-07-04 12:54:27 +0100

    remote jQuery

--- docs/popup1.php
+++ docs/popup1.php
@@ -5,9 +5,9 @@
 <meta name="description" content="File Manager">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="Richard Keasley">
-<script src="/INC/jquery/jquery.min.js"></script>
-<script src="/INC/jquery/jquery-ui.min.js"></script>
-<link href="/INC/jquery/jquery-ui.min.css" rel="stylesheet" type="text/css" media="all">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<link href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" media="all">
 <link href="/INC/icofont/style.css" rel="stylesheet" type="text/css" media="all">
 
 <title>File Manager - popup 1</title>
