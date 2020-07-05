eb55b3092ae73a9a48ec82bef2a8a9e5320e704a
richard-keasley <67803679+richard-keasley@users.noreply.github.com>
2020-07-05 17:50:01 +0100

    documentation

--- docs/tiny.php
+++ docs/tiny.php
@@ -43,7 +43,31 @@ $(function() {
 </script>
 
 <pre>
-Add source code once it works
+&lt;textarea class="html"&gt;&lt;/textarea&gt;
+&lt;script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.4.0/tinymce.min.js"&gt;&lt;/script&gt;
+&lt;?php include_once dirname(__DIR__) . '/include.php';?&gt;
+&lt;script&gt;
+$(function() {
+	tinymce.init({
+		selector:'textarea[class=html]',
+		toolbar: "undo redo | bold italic | alignleft aligncenter alignright alignjustify | link image FileMan",
+		setup: function(editor) { 
+			editor.ui.registry.addButton('FileMan', {
+				text: 'FileMan', // the text to display alongside the button
+				icon: 'image', // the icon to display alongside the button
+				onAction: function(_) {
+					var fileman_element = FileMan.init();
+					FileMan.get_file = function(listitem) {
+						editor.insertContent(FileMan.item.html(listitem));
+						fileman_element.dialog('close');
+					};
+					fileman_element.dialog('open');
+				}
+			})
+		}
+	})
+});
+&lt;/script&gt;
 </pre>
 
 <p class="nav"><a class="icon-undo" href=".">back</a></p>
