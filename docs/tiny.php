a9cee4c0b93845b1745fd2258252742752b1e48a
richard-keasley <67803679+richard-keasley@users.noreply.github.com>
2020-07-06 09:10:09 +0100

    Update tiny.php

--- docs/tiny.php
+++ docs/tiny.php
@@ -23,7 +23,7 @@
 $(function() {
 	tinymce.init({
 		selector:'textarea[class=html]',
-		toolbar: "undo redo | bold italic | alignleft aligncenter alignright alignjustify | link image FileMan",
+		toolbar: "undo redo | link image FileMan",
 		setup: function(editor) { 
 			editor.ui.registry.addButton('FileMan', {
 				text: 'FileMan', // the text to display alongside the button
