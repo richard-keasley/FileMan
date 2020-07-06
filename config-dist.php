d0000fac2e0caa88219de0b14d817184a4e50261
richard-keasley <67803679+richard-keasley@users.noreply.github.com>
2020-07-06 09:05:35 +0100

    Create config-dist.php

---
+++ config-dist.php
@@ -0,0 +1,28 @@
+<?php
+/* add any site requirements here */
+# require_once '{all the files your site needs}';
+
+/* permissions */
+if('user has permission to read files') {
+	fm_settings::$perms[] = 'get_dir';
+	fm_settings::$perms[] = 'get_file';
+}
+if('user has permission to edit files') {
+	fm_settings::$perms[] = 'add_file';
+	fm_settings::$perms[] = 'del_file';
+}
+if('user is a site admin') {
+	fm_settings::$perms[] = 'add_dir';
+	fm_settings::$perms[] = 'del_dir';
+	fm_settings::$perms[] = 'get_settings';
+}
+
+// website root path for generating HTML links
+fm_settings::$url_root = $_SERVER['DOCUMENT_ROOT']; 
+
+// base path for FileMan browsing
+fm_settings::$fm_root  = {path to your files}; 
+
+// upload_types [label] = extensions
+fm_settings::$upload_types['image'] = 'jpg,gif,png';
+fm_settings::$upload_types['pdf'] = 'pdf';
