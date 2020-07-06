d78e98867afa7da8dd5cc22787cf8f3796548d1e
Richard Keasley <67803679+richard-keasley@users.noreply.github.com>
2020-07-06 09:42:11 +0100

    User config

--- settings.php
+++ settings.php
@@ -1,4 +1,11 @@
 <?php 
+// user config 
+$include = dirname(__DIR__) . '/fileman.config.php';
+if(file_exists($include)) include $include;
+
+// Allow those who can get_file to preview_file
+if(in_array('get_file', fm_settings::$perms)) fm_settings::$perms[] = 'preview_file';
+
 class fm_settings {
 const json_flags = JSON_INVALID_UTF8_SUBSTITUTE | JSON_PARTIAL_OUTPUT_ON_ERROR;
 static $url_root = '';  // website root path
@@ -41,10 +48,3 @@ static function perms($perm_name) {
 }
 	
 } // class fm_settings
-
-// user config 
-$include = __DIR__ . '/config.php';
-if(file_exists($include)) include $include;
-
-// Allow those who can get_file to preview_file
-if(in_array('get_file', fm_settings::$perms)) fm_settings::$perms[] = 'preview_file';
